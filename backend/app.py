b58b3824116083f1275b7e22de688a61b2df7bdb
SAHIL VIKAS THORAT <Sahil@T-2.local>
2025-08-12 14:06:49 +0530

    Add static folder and fix static mount path in app.py

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,7 @@ import io
 import chardet
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
+import os
 
 app = FastAPI()
 
@@ -23,7 +24,9 @@ app.add_middleware(
 )
 
 # Serve static files from /static
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
 
 class TextData(BaseModel):
     text: str
